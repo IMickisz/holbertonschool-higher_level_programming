743d9a05e332e49816e3ee0e6f359d94b5415bfb
Ivan Mickisz <ivan.mickisz3@gmail.com>
2022-01-18 07:54:32 +0100

    added readme, added last line

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    result = 0
+    try:
+        if b != 0:
+            result = a / b
+            return result
+        else:
+            result = None
+    except ValueError:
+        pass
+    finally:
+        print("Inside result: {}".format(result))
