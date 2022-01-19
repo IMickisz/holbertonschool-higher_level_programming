417452ce87ba9bc5dfd1045a507ac473202ea1eb
Ivan Mickisz <ivan.mickisz3@gmail.com>
2022-01-19 12:38:31 +0100

    added task 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+"""5-square.py
+
+Write a class Square that defines a square by:
+
+- Private instance attribute: size
+property def size(self): to retrieve it
+property setter def size(self, value): to set it:
+> size must be an integer, otherwise raise a TypeError exception with the
+messagesize must be an integer
+> if size is less than 0, raise a ValueError exception with the message size
+must be >= 0
+
+- Instantiation with optional size: def __init__(self, size=0):
+
+- Public instance method: def area(self): that returns the current square area
+
+- Public instance method: def my_print(self): that prints in stdout the square
+with the character #:
+if size is equal to 0, print an empty line
+"""
+
+
+class Square:
+    """class that defines a square"""
+    """The __init__ method is run as soon as the object class is instantiated.
+
+    Args:
+    size: The size of the square.
+
+    Attribute:
+    __size: (Private instance attribute) size of the square.
+    """
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError('size must be an integer')
+        elif value < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = value
+
+    def area(self):
+        """This method returns the current square area"""
+        return self.__size ** 2
+
+    def my_print(self):
+         if self.__size == 0:
+             print("")
+         else:
+             for i in range(0, self.__size):
+                 print("#" * self.__size)
