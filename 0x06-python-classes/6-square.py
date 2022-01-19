430dd1e616c29c362022835429f2a8524a5866d2
Ivan Mickisz <ivan.mickisz3@gmail.com>
2022-01-19 15:17:49 +0100

    changed myprint

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -42,7 +42,17 @@ class Square:
     """
     def __init__(self, size=0, position=(0, 0)):
         self.__size = size
+        if type(value) != int:
+            raise TypeError('size must be an integer')
+        elif value < 0:
+            raise ValueError('size must be >= 0')
         self.position = position
+        if type(value) != tuple or len(value) != 2:
+            raise TypeError("position must be a 'tuple of 2' positive integers")
+        elif type(value[0]) != int or type(value[1]) != int:
+            raise TypeError("position must be a tuple of 2 positive 'integers'")
+        elif value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 'positive' integers")
 
     @property
     def size(self):
