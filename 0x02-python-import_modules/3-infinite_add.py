137b1c3bd403b2665b1d585e6dcc4c44afd3f128
Ivan Mickisz <ivan.mickisz3@gmail.com>
2022-01-25 11:27:06 +0100

    corrected space

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,8 +4,8 @@ from sys import argv
 if __name__ == "__main__":
     if len(argv) == 1:
         print("0")
-        if len(argv) > 1:
-            add_argument = 0
-            for argument in range(1, len(argv)):
-                add_argument += int(argv[argument])
-            print("{:d}".format(add_argument))
+    if len(argv) > 1:
+        add_argument = 0
+        for argument in range(1, len(argv)):
+            add_argument += int(argv[argument])
+        print("{:d}".format(add_argument))
