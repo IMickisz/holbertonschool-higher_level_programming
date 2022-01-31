a12cc4b2f64a7c8a68eb83c3bc8da586a1c8d5fe
Ivan Mickisz <ivan.mickisz3@gmail.com>
2022-01-31 21:44:57 +0100

    added description module

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+"""
+0-lookup.py
+You are not allowed to import any module.
+Function: lookup.
+"""
+
+
 def lookup(obj):
     """
     Returns the list of available attributes and methods of an object
