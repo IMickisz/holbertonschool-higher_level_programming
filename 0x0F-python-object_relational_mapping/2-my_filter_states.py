8f586569f932f58c3f070f93cdfb03a21d0240e5
Ivan Mickisz <ivan.mickisz3@gmail.com>
2022-04-13 16:18:13 +0200

    added charset

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     from sys import argv
 
     db = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3])
+                         passwd=argv[2], db=argv[3], charset="utf8")
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id ASC"
                    .format(argv[4]))
