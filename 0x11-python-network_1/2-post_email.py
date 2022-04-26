323df899aecdeb42b767bd22474125ec9088715b
Ivan Mickisz <ivan.mickisz3@gmail.com>
2022-04-26 12:17:44 +0200

    added task 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -5,12 +5,12 @@ in utf-8)"""
 
 
 if __name__ == "__main__":
-    import urllib
-    import sys
+    from urllib import parse, request
+    from sys import argv
 
-    values = {'email':  sys.argv[2]}
-    data = urllib.parse.urlencode(values)
+    values = {'email': argv[2]}
+    data = parse.urlencode(values)
     data = data.encode('ascii')
-    req = urllib.request.Request(sys.argv[1], data)
+    req = request.Request(sys.argv[1], data)
     with urllib.request.urlopen(req) as response:
         print(response.read().decode("utf-8", "replace"))
