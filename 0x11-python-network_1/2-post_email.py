0e1ceb78227144d930949ffd2effc17a400e3eea
Ivan Mickisz <ivan.mickisz3@gmail.com>
2022-04-26 12:04:35 +0200

    corrected syntax

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -5,10 +5,10 @@ in utf-8)"""
 
 
 if __name__ == "__main__":
-    import urllib.request
+    import urllib
     import sys
 
-    values = {'email' = sys.argv[2]}
+    values = {'email':  sys.argv[2]}
     data = urllib.parse.urlencode(values)
     data = data.encode('ascii')
     req = urllib.request.Request(sys.argv[1], data)
